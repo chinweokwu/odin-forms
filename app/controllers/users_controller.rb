469cd631206e9026dba6d4d291364ff3090ec871
chinweokwu <paulcmorah@gmail.com>
2020-08-07 13:59:58 +0100

    added edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
     def index
+        @users = User.all
+    end
 
+    def show
+        @user = User.find(params[:id])
     end
 
     def new
@@ -8,12 +12,26 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = USer.new(user_params)
+        @user = User.new(user_params)
 
         if @user.save
             redirect_to @user
         else
             render 'new'
+        end
+    end
+
+    def edit
+        @user = User.find(params[:id]) 
+    end
+
+    def update
+        @user = User.find(params[:id])
+
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render "edit"
         end
     end
 
